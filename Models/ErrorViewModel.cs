fec2782b0bbc819e26574de3dcfa5acf4efd79f6
Chanthy Ngin <nginchanthy@gmail.com>
2018-04-07 11:12:31 -0700

    init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dappertest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
